9a224a88e1f4820a4fedeb73ce3b4b3988c07def
Carlos <Carllos.victor@hotmail.com>
2024-07-24 11:57:58 -0300

    primeiros passos da integração da API

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weather, setWeather] = useState({})
   const inputRef = useRef()
 
 
-function searchCity(){
-  console.log(inputRef.current.valor)
+  async function searchCity(){
   const city = inputRef.current.value
+
   const key = "ca47ff4ed5b53d8598e9ae1db2b5a916"
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-  const data = axios.get(url)
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+
+  const apiDataseWeather = await axios.get(url)
+  setWeather(apiDataseWeather)
+  
+  
+
 }
 
 return (
