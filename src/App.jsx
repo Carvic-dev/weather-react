4ddad3a98a5aec6f5ebad007619d9f11d9dabbce
Carlos <Carllos.victor@hotmail.com>
2024-07-26 19:50:49 -0300

    Implementação da API (openweater), Edição de arquivos css (implementação do layout)

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import { useRef, useState, userRef } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import Weatherinformations from './components/Weatherinformations/Weatherinformations'
 
 function App() {
-  const [weather, setWeather] = useState({})
+  const [weather, setWeather] = useState()
   const inputRef = useRef()
 
 
   async function searchCity(){
-  const city = inputRef.current.value
+    const city = inputRef.current.value
 
-  const key = "ca47ff4ed5b53d8598e9ae1db2b5a916"
+    const key = "ca47ff4ed5b53d8598e9ae1db2b5a916"
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
 
-  const apiDataseWeather = await axios.get(url)
-  setWeather(apiDataseWeather)
+    const appInfo = await axios.get(url)
+    setWeather(appInfo.data)
   
-  
-
 }
 
 return (
-  <div>
-  <h1> Previsão do tempo-react</h1>
-  <input ref={inputRef} type="text" placeholder='Digite o nome da cidade'/>
-  <button onClick={searchCity}>Buscar</button>
+  <div class='container-main'>
+    <div> <h1> Previsão do Tempo </h1>
+    <input ref={inputRef} type="text" placeholder='Digite o nome da cidade'/>
+    <button onClick={searchCity}>Buscar</button></div>
+
+    <div class='container-more-info'>{ weather && <Weatherinformations weather={weather}/>}</div>
   </div>
   )
 }
---
+++ src/components/Weatherinformations/Weatherinformations.jsx
@@ -0,0 +1,32 @@
+
+import './Weatherinformations.css'
+
+function Weatherinformations({ weather }) {
+  console.log(weather)
+  return (
+    <div class='weater-main'>
+      <h3 class='location'><img src="/src/assets/icons/location.svg" alt="icone pin" />{weather.name}</h3>
+
+      <div class='weater-icon'>
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="icone que ilustra o clima atual"
+        />
+      </div>
+      <div class='weather-temp-now'>
+        <p class='temp-C'>{Math.round(weather.main.temp)}<p>°C</p></p>
+        <p class='description'>{weather.weather[0].description}</p></div>
+
+      <div class="weater-statics">
+        <div class='weater-info-01'>
+        <p>Sensação térmica: {Math.round(weather.main.feels_like)} </p> </div>
+        <div class='weater-info-02'>
+        <p>Umidade: {weather.main.humidity} </p> </div>
+        <div class='weater-info-03'>
+        <p>Pressão: {weather.main.pressure} </p> </div>
+      </div>
+    </div>
+  );
+}
+
+export default Weatherinformations;
