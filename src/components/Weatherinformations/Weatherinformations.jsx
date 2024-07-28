44342f6789eef49c70f54eea3df10dbc8c9dffb3
Carlos <Carllos.victor@hotmail.com>
2024-07-28 14:48:59 -0300

    finalização do Css, preparação para adição de novas features e resposividade

--- src/components/Weatherinformations/Weatherinformations.jsx
+++ src/components/Weatherinformations/Weatherinformations.jsx
@@ -1,29 +1,39 @@
-
-import './Weatherinformations.css'
+import "./Weatherinformations.css";
 
 function Weatherinformations({ weather }) {
-  console.log(weather)
+  console.log(weather);
   return (
     <section class="main-info">
-      <h3 class='location'><img src="/src/assets/icons/location.svg" alt="icone pin" />{weather.name}</h3>
+      <h3>
+        <img src="/src/assets/icons/pin.png" alt="icone pin" />
+        {weather.name}
+      </h3>
+      <div class="temp-now-info">
+        <div class="weather-icon">
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+            alt="icone que ilustra o clima atual"
+          />
+        </div>
 
-      <div class='weater-icon'>
-        <img
-          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-          alt="icone que ilustra o clima atual"
-        />
+        <div class="weather-temp-now">
+          <p class="temp-C">
+            {Math.round(weather.main.temp)}
+            <p class="simbol">°C</p>
+          </p>
+          <p class="description">{weather.weather[0].description}</p>
+        </div>
       </div>
-      <div class='weather-temp-now'>
-        <p class='temp-C'>{Math.round(weather.main.temp)}<p>°C</p></p>
-        <p class='description'>{weather.weather[0].description}</p></div>
-
-      <div class="weater-statics">
-        <div class='weater-info-01'>
-        <p>Sensação térmica: {Math.round(weather.main.feels_like)} </p> </div>
-        <div class='weater-info-02'>
-        <p>Umidade: {weather.main.humidity} </p> </div>
-        <div class='weater-info-03'>
-        <p>Pressão: {weather.main.pressure} </p> </div>
+      <div class="weather-statics">
+        <div class="weather-info">
+          <p>Sensação térmica: <br /><span>{Math.round(weather.main.feels_like)} <p>°C</p> </span></p>{" "}
+        </div>
+        <div class="weather-info">
+          <p>Umidade: <br /> <span>{weather.main.humidity} <p>%</p> </span> </p>{" "}
+        </div>
+        <div class="weather-info">
+          <p>Pressão: <br /> <span>{weather.main.pressure} <p>N/m²</p> </span>  </p>{" "}
+        </div>
       </div>
     </section>
   );
